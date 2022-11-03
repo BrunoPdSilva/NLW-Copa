20c897a53cfb91b856c4e394f1988a7664f331b2
Bruno Peres <bruno_16515@hotmail.com>
2022-11-03 17:06:36 -0300

    :rocket: Backend finalizado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt"
 
 import { poolRoutes } from "./routes/pool";
 import { authRoutes } from "./routes/auth";
@@ -14,6 +15,10 @@ async function bootstrap() {
 
   await fastify.register(cors, {
     origin: true,
+  });
+
+  await fastify.register(jwt, {
+    secret: "nlwcopa",
   });
 
   await fastify.register(poolRoutes)
