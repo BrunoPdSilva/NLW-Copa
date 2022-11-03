66365c49990aa933f96b3242e07f0cb6892d3200
Bruno Peres <bruno_16515@hotmail.com>
2022-11-03 15:23:23 -0300

    :rocket: Aula 04 - Separando rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+import { poolRoutes } from "./routes/pool";
+import { authRoutes } from "./routes/auth";
+import { gameRoutes } from "./routes/game";
+import { guessRoutes } from "./routes/guess";
+import { userRoutes } from "./routes/user";
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -15,12 +16,12 @@ async function bootstrap() {
     origin: true,
   });
 
-  fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count();
-
-    return { count };
-  });
-
+  await fastify.register(poolRoutes)
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(userRoutes)
+  
   await fastify.listen({ port: 3333, /* host: "0.0.0.0" */ });
 }
 
